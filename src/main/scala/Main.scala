c5a52489c4ba644d337dbf6dfb3917c6d17313fd
Devon Stewart <dstewart@tubi.tv>
2022-06-25 13:02:13 -0700

    Scala 3 attempt

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,13 +1,22 @@
 package help
 
 object Main extends App {
-  def createLogic = new Logic[Instance] {
-    def build(value: String): Data[Instance] = Data[Instance](value)
-    def func(data: Data[Instance]): String = data.value
+  val I = new Instance
+  val Co = new Container(I)
+
+  def createLogic = new Logic(I, Co) {
+    def build(value: String): C.Data = C.Data(value)
+    def func(data: C.Data): String = data.value
   }
 
   val logic1 = createLogic
   val logic2 = createLogic
 
+  // Does not compile due to
+  //
+  //   [error]  found   : help.Main.logic1.C.Data
+  //   [error]  required: help.Main.logic2.C.Data
+  //
+  // despite the underlying instance of C being bound by the same `Instance`
   assert(logic2.func(logic1.build("Hello, world!")) == "Hello, world!")
 }
